dd7faf7b41685e0c24e0f51081c252107ca2b24a
xx <xingxiang@twotreesgroup.com>
2021-02-26 00:07:46 +0800

    added routing skeleton

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import CreateExercise from "./components/CreateExercise";
+import CreateUser from "./components/CreateUser";
+import EditExercise from "./components/EditExercise";
+import ExercisesList from "./components/ExercisesList";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <Route path="/" exact component={ExercisesList} />
+      <Route path="/edit/:id" component={EditExercise} />
+      <Route path="/create" exact component={CreateExercise} />
+      <Route path="/user" exact component={CreateUser} />
+    </Router>
   );
 }
 
